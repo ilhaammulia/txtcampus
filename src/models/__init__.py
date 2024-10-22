ee82bb8ec749b6bde83eb358367317c143639599
Ilham Mulia <dev.ilhammulia@gmail.com>
2024-10-22 19:38:29 +0700

    fix(model): fix import db

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,8 @@
 from datetime import datetime
+from src.database import db
 
-def get_db():
-    from src.app import db
-    return db
-
-class BaseModel(get_db().Model):
+class BaseModel(db.Model):
     __abstract__ = True  # This ensures that BaseModel is not created as a table
 
-    created_at = get_db().Column(get_db().DateTime, default=datetime.utcnow)
-    updated_at = get_db().Column(get_db().DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
